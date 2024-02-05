f14a5e4e2799db1749b9c4f7f8767ddf5ace6e58
Meerim Kerkibasheva <mkerkibasheva@gmail.com>
2024-02-06 01:13:59 +0600

    corrected array images

--- src/utils/constants/index.js
+++ src/utils/constants/index.js
@@ -1,9 +1,10 @@
 export const IMAGES = [
-  "https://images.unsplash.com/photo-1702560030824-02cf6c4761ef?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDc5fGJvOGpRS1RhRTBZfHxlbnwwfHx8fHw%3D",
-  "https://images.unsplash.com/photo-1705852592864-1a31a6c9260a?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDQwfGJvOGpRS1RhRTBZfHxlbnwwfHx8fHw%3D",
-  "https://images.unsplash.com/photo-1554310603-d39d43033735?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDE0MnxibzhqUUtUYUUwWXx8ZW58MHx8fHx8",
-  "https://images.unsplash.com/photo-1704225982484-b6784500ace9?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDEzNXxibzhqUUtUYUUwWXx8ZW58MHx8fHx8",
-  "https://images.unsplash.com/photo-1703016445632-240f009aa738?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDI2NnxibzhqUUtUYUUwWXx8ZW58MHx8fHx8",
-  "https://images.unsplash.com/photo-1681296363726-45c9ca32abde?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDE0OXxibzhqUUtUYUUwWXx8ZW58MHx8fHx8",
-  "https://plus.unsplash.com/premium_photo-1698429260304-56c5a60f419b?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDE3NXxibzhqUUtUYUUwWXx8ZW58MHx8fHx8",
+  "https://img.freepik.com/free-photo/majestic-mountain-peak-tranquil-meadow-panoramic-landscape-generated-by-ai_188544-18394.jpg?w=2000&t=st=1706812616~exp=1706813216~hmac=2da8a54c519a1f19cc1cc0996ad96d3659eefa5f56f3713fb6bff61709926d2c",
+  "https://img.freepik.com/free-photo/volcano-with-mist-sunset_1150-18322.jpg?w=1800&t=st=1706812421~exp=1706813021~hmac=db0c0c1cdc99088371e160992d8c0dfd22f728b6f426a74ca5ca2c81c576aa52",
+  "https://img.freepik.com/free-photo/beautiful-shot-snowy-mountain-sunset_181624-37878.jpg?size=626&ext=jpg&uid=R110393921&ga=GA1.1.284647328.1701093593&semt=ais",
+  "https://img.freepik.com/free-photo/breathtaking-shot-beautiful-stones-turquoise-water-lake-hills-background_181624-12847.jpg?w=1800&t=st=1706812458~exp=1706813058~hmac=78115392edd321a6e4d44e9fa070c268c057e93b9dc34bb47960b03083e1f1c1",
+  "https://img.freepik.com/free-photo/nature-beauty-captured-tranquil-scene-mountain-peak-reflection-generative-ai_188544-12745.jpg?size=626&ext=jpg&uid=R110393921&ga=GA1.1.284647328.1701093593&semt=ais",
+  "https://img.freepik.com/free-photo/snowy-mountain-peak-starry-galaxy-majesty-generative-ai_188544-9650.jpg?size=626&ext=jpg&uid=R110393921&ga=GA1.1.284647328.1701093593&semt=ais",
+  "https://img.freepik.com/free-photo/black-background-with-leaves-vegetation-texture_23-2149872513.jpg?size=626&ext=jpg&uid=R110393921&ga=GA1.1.284647328.1701093593&semt=sph",
+  "https://img.freepik.com/free-photo/snowy-mountain-peak-cloudy-day_181624-56144.jpg?size=626&ext=jpg&uid=R110393921&ga=GA1.1.284647328.1701093593&semt=sph",
 ];
